7b3cfcaa4f6747e289ad9ef42b3efb106dd99d6f
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-17 21:24:59 +0200

    + Gemeindegrenzen

---
+++ dataset/gemeindegrenzen/README.md
@@ -0,0 +1,5 @@
+Die Gemeindegrenzen beruhen auf den Daten der amtlichen Vermessung
+
+https://opendata.swiss/de/dataset/gemeindegrenzen
+
+http://www.geolion.zh.ch/geodatensatz/865 
---
+++ scripts/plot.r
@@ -0,0 +1,14 @@
+kt_zh_data_root = paste0(Sys.getenv('github_root'),  '/open-data.Kanton-Zuerich/')
+
+x11()
+library(rgdal)
+
+UP_KANTON_F         <- readOGR(paste0(kt_zh_data_root, 'dataset/gemeindegrenzen'), 'UP_KANTON_F'        )
+UP_GEMEINDEN_F      <- readOGR(paste0(kt_zh_data_root, 'dataset/gemeindegrenzen'), 'UP_GEMEINDEN_F'     )
+UP_GEMEINDEN_L      <- readOGR(paste0(kt_zh_data_root, 'dataset/gemeindegrenzen'), 'UP_GEMEINDEN_L'     )
+UP_GEMEINDEN_SEEN_F <- readOGR(paste0(kt_zh_data_root, 'dataset/gemeindegrenzen'), 'UP_GEMEINDEN_SEEN_F')
+
+plot(UP_KANTON_F        )
+plot(UP_GEMEINDEN_F     )
+plot(UP_GEMEINDEN_L     )
+plot(UP_GEMEINDEN_SEEN_F)
